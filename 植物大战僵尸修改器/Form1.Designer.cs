d434cfc88d1bfaf506fa4fbb42e2e95d9638011d
1364876516 <1364876516@qq.com>
2020-09-07 01:15:39 +0800

    初版

---
+++ 植物大战僵尸修改器/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace 植物大战僵尸修改器
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(68, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "未检测到游戏运行";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(12, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "修改阳光数值";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox1.Location = new System.Drawing.Point(176, 51);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(75, 20);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "无冷却";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkBox2.Location = new System.Drawing.Point(176, 73);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(107, 20);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "锁阳光数量";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 30);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "输入阳光数量";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(282, 130);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "植物大战僵尸修改器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ 植物大战僵尸修改器/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 植物大战僵尸修改器
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txt_light_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !(e.KeyChar == 8 || e.KeyChar >= '0' && e.KeyChar <= '9');
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int ReadProcessMemory(int hProcess, int lpBaseAddress, ref int buffer, int size, int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, ref int lpBuffer, int nSize, int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern int OpenProcess(int processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowThreadProcessId(int hWnd, ref int lpdwProcessId);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("kernel32.dll")]
+        public static extern int CloseHandle(int hObject);
+
+
+        public const int PROCESS_ALL_ACCESS = 2035711;
+        private int ppid;
+        private int hwnd;
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ppid = int.MinValue;
+            hwnd = FindWindow(null, "植物大战僵尸中文版");
+            if (hwnd != 0)
+            {
+                label1.Text = "已检测到游戏运行";
+            }
+            GetWindowThreadProcessId(hwnd, ref ppid); // 取与窗口关联的进程标识
+            int hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, ppid); // 进程句柄
+            if (checkBox2.Checked == true)
+            {
+                int esi = 0, edi = 0;
+                // 基址(base_addr) 007794F8
+                ReadProcessMemory(hProcess, 0x6a9ec0, ref esi, 4, 0);
+                // mov edi,[esi+00000868]
+                ReadProcessMemory(hProcess, esi + int.Parse("768", NumberStyles.HexNumber), ref edi, 4, 0);
+                // mov [edi+00005578],esi
+                int lightSize = Convert.ToInt32(textBox1.TextLength <= 0 ? "0" : textBox1.Text); // 阳光大小
+                WriteProcessMemory(hProcess, edi + int.Parse("5560", NumberStyles.HexNumber), ref lightSize, 4, 0); // 修改阳光
+            }
+            CloseHandle(hProcess); // 关闭内核对象
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkBox2.Checked = false;
+            ppid = int.MinValue;
+            hwnd = FindWindow(null, "植物大战僵尸中文版");
+            GetWindowThreadProcessId(hwnd, ref ppid); // 取与窗口关联的进程标识
+            int hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, ppid); // 进程句柄
+            int esi = 0, edi = 0;
+            // 基址(base_addr) 007794F8
+            ReadProcessMemory(hProcess, 0x6a9ec0, ref esi, 4, 0);
+            // mov edi,[esi+00000868]
+            ReadProcessMemory(hProcess, esi + int.Parse("768", NumberStyles.HexNumber), ref edi, 4, 0);
+            // mov [edi+00005578],esi
+            int lightSize = Convert.ToInt32(textBox1.TextLength <= 0 ? "0" : textBox1.Text); // 阳光大小
+            WriteProcessMemory(hProcess, edi + int.Parse("5560", NumberStyles.HexNumber), ref lightSize, 4, 0); // 修改阳光
+            CloseHandle(hProcess); // 关闭内核对象
+            checkBox2.Checked = true;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                ppid = int.MinValue;
+                hwnd = FindWindow(null, "植物大战僵尸中文版");
+                GetWindowThreadProcessId(hwnd, ref ppid); // 取与窗口关联的进程标识
+                int hProcess = OpenProcess(PROCESS_ALL_ACCESS, false, ppid); // 进程句柄
+                int a = 0x1477;
+                WriteProcessMemory(hProcess, 0x00487296, ref a, 2, 0);
+            }
+        }
+    }
+}
